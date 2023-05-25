f4d5739bcb67e414f8cfc963d066ab3cadcd1ef2
erdincnamal <65330863+erdincnamal@users.noreply.github.com>
2023-05-25 20:21:24 +0300

    v1.1
    Otomatik size ayarlama ve tarama hızlarının değişimi için uygun bölüm eklendi.

--- NEDALcontributionValues.m
+++ NEDALcontributionValues.m
@@ -1,12 +1,28 @@
-nedalData = table2struct(erdincxlsx);
+% Import Data'ya tıkladıktan sonra önce sütunların "Number" formatında seçildiğine emin olun.
+% Sonra Number formatının üzerinde yazan isim sıralamasının VarName1 ile başlayıp VarName10 ile bittiğinden emin olun. (Aradakiler de bu sırada olmalı)
+% Bu adımlardan istediğiniz dataları seçip, ardından scanRate ve dosya çıktı ismini ayarlayarak çalıştırabilirsiniz.
 
-srScanV01 = 0.1^(1/2);
-srScanV02 = 0.2^(1/2);
-srScanV05 = 0.5^(1/2);
-srScanV1 = 1;
-srScanV2 = 2^(1/2);
 
-data_number = 2604;
+% Sağ tarafta import ettiğiniz tablonun ismi yazmaktadır onu direkt buraya değiştirmeden yazınız.
+nedalData = table2struct(cvexcel2);
+
+
+% Data sayısını artık girmenize gerek yok.
+data_number = size(nedalData,1);
+
+% TARAMA HIZLARI İÇİN SADECE BURADA EŞİTLİĞİN SAĞ TARAFINDAKİ SAYILARI DEĞİŞTİRİN. ONDALIK İÇİN NOKTA KULLANIN.
+scanRate1=0.1; 
+scanRate2=0.2;
+scanRate3=0.5;
+scanRate4=0.7;
+scanRate5=1;
+% -------------------------------------------------------------------------------------------------------------
+
+srScanV01 = scanRate1^(1/2);
+srScanV02 = scanRate2^(1/2);
+srScanV05 = scanRate3^(1/2);
+srScanV1 = scanRate4^(1/2);
+srScanV2 = scanRate5^(1/2);
 
 
 k1_values = zeros(data_number,1);
@@ -33,27 +49,27 @@ k1_values(i)=lineer.Coefficients.Estimate(2);
 k2_values(i)=lineer.Coefficients.Estimate(1);
 end
 
-scanV01_k1 = k1_values.*0.1;
+scanV01_k1 = k1_values.*scanRate1;
 scanV01_k2 = k2_values*srScanV01;
 fittingCurrentV01=scanV01_k1+scanV01_k2;
 
-scanV02_k1 = k1_values.*0.2;
+scanV02_k1 = k1_values.*scanRate2;
 scanV02_k2 = k2_values.*srScanV02;
 fittingCurrentV02=scanV02_k1+scanV02_k2;
 
-scanV05_k1 = k1_values.*0.5;
+scanV05_k1 = k1_values.*scanRate3;
 scanV05_k2 = k2_values.*srScanV05;
 fittingCurrentV05=scanV05_k1+scanV05_k2;
 
-scanV1_k1 = k1_values.*1;
+scanV1_k1 = k1_values.*scanRate4;
 scanV1_k2 = k2_values.*srScanV1;
 fittingCurrentV1=scanV1_k1+scanV1_k2;
 
-scanV2_k1 = k1_values.*2;
+scanV2_k1 = k1_values.*scanRate5;
 scanV2_k2 = k2_values.*srScanV2;
 fittingCurrentV2=scanV2_k1+scanV2_k2;
 
-label=["Tarama Hızı","K1","K2","K1-0.1","K2-0.1","Fitting Current","Tarama Hızı 0.2","K1-0.2","K2-0.2","Fitting Current","Tarama Hızı 0.5","K1-0.5","K2-0.5","Fitting Current","Tarama Hızı 1.0","K1-1.0","K2-1.0","Fitting Current","Tarama Hızı 2.0","K1-2.0","K2-2.0","Fitting Current"];
+label=["Tarama Hızı","K1","K2","K1-"+scanRate1,"K2-"+scanRate1,"Fitting Current","Tarama Hızı "+scanRate2,"K1-"+scanRate2,"K2-"+scanRate2,"Fitting Current","Tarama Hızı "+scanRate3,"K1-"+scanRate3,"K2"+scanRate3,"Fitting Current","Tarama Hızı "+scanRate4,"K1-"+scanRate4,"K2-"+scanRate4,"Fitting Current","Tarama Hızı "+scanRate5,"K1-"+scanRate5,"K2-"+scanRate5,"Fitting Current"];
 
 outputValues=zeros(data_number,22);
 
@@ -90,10 +106,15 @@ end
 outputData=struct;
 outputData.labels=label;
 outputData.values=outputValues;
-filename = 'testdata2.xlsx';
+
+% Output dosyasının ismini tırnak işaretlerini ve .xlsx 'i silmeden değiştirin.
+filename = 'FILENAME.xlsx';
 
 
 writematrix(outputData.labels,filename)
 writematrix(outputData.values,filename,'WriteMode','append')
 
+plot(outputValues(:,1),outputValues(:,4))
+xlabel('Time'), ylabel('K values')
+title('cv')
 
